89169e90d272f7cd139e5fe96efc01c2281dd11e
sajieun <sajieun000721@naver.com>
2024-05-21 18:00:04 +0900

    db 없이 저장된 값 불러오기

---
+++ src/main/java/com/example/exception/controller/UserApiController.java
@@ -0,0 +1,56 @@
+package com.example.exception.controller;
+
+import com.example.exception.model.Api;
+import com.example.exception.model.UserResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+// DB가 없을 때 직접 생성
+    private static List<UserResponse> userList = List.of(
+            UserResponse.builder()
+                    .id("1")
+                    .age(10)
+                    .name("홍길동")
+                    .build()
+        ,
+        UserResponse.builder()
+                .id("2")
+                .age(10)
+                .name("유관순")
+                .build()
+    );
+
+    @GetMapping("/id/{userId}")
+    public Api<UserResponse> getUser(
+            @PathVariable String userId
+    ){
+        var user = userList.stream()
+                .filter(
+                        it -> it.getId().equals(userId)
+                )
+                .findFirst()
+                .get();
+
+        Api<UserResponse> response = Api.<UserResponse>builder()
+                .data(user)
+                .build();
+        return response;
+    }
+//    결과값
+
+    //"result_code": null,
+    //        "result_message": null,
+    //        "data":{
+    //        "id": "1",
+    //                "name": "홍길동",
+    //                "age": 10
+    //    }
+}
+}
---
+++ src/main/java/com/example/exception/model/Api.java
@@ -0,0 +1,20 @@
+package com.example.exception.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Builder
+// 제네릭 타입
+public class Api<T> {
+    private  String resultCode;
+    private String resultMessage;
+    private T data;
+}
